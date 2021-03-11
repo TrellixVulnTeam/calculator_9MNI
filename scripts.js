731b651f2a7382ecdaa6d499cf76844b2d3003db
Evan Kwan <evanyuekwan@gmail.com>
2021-03-11 17:36:10 -0500

    update js

--- scripts.js
+++ scripts.js
@@ -12,6 +12,7 @@ app.display = 0;
 // cached selectors
 app.$button = $('[class*="-button"]');
 app.$display = $('.display');
+app.$totalContainer = $('.total-container');
 
 app.getNumber = () => {
   app.$button.on('click', function() {
@@ -28,11 +29,34 @@ app.getNumber = () => {
       }
       app.$display.text(display);
     }
+    app.checkDisplayLength();
   });
+}
+
+app.checkDisplayLength = () => {
+  let displayText = app.$display.text();
+  console.log(typeof displayText);
+  let number = parseInt(displayText);
+  let sequence;
+
+  if (number > 99999999) {
+    app.$totalContainer.addClass( 'display-size-4' );
+  } else if (number > 9999999) {
+    app.$totalContainer.addClass( 'display-size-3' );
+  } else if (number > 999999) {
+    app.$totalContainer.addClass( 'display-size-2' );
+  }
+
+  if (displayText.length > 1 && displayText.length % 3 === 1) {
+    displayText = displayText.substring(0, 1) + ',' + displayText.substring(1, displayText.length);
+    app.$display.text(displayText);
+  }
+  
 }
 
 app.init = () => {
   app.getNumber();
+  app.checkDisplayLength();
 }
 
 $.ready = () => {
